e1aa688d9131348906148df9f8a8e0421835adc5
kazuya-akehi <xakehi@booklista.co.jp>
2025-07-13 12:20:38 +0900

    add

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
     ];
 
     /**
