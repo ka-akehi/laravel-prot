113c78d61f1380db6a759e03fa0c52a547aaa740
kazuya-akehi <xakehi@booklista.co.jp>
2025-09-14 10:53:37 +0900

    add

---
+++ app/Console/Commands/TxnReadUncommittedTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TxnReadUncommittedTest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:txn-read-uncommitted-test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
